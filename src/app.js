5bc8fc80bdb22ed15228d19b5aaed355fb0eb9b7
ArunKumar165 <vk0799027@gmail.com>
2025-06-30 21:34:12 +0530

    day1 get,post

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const express=require("express");
 const app=express();
-app.use("/",(req,res)=>{
-res.send("this is home ")
+
+
+
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Arun",lastName:"kumar"})
+})
+
+
+app.post("/user",(req,res)=>{
+    res.send("data is added successfully")
 })
 
 app.use("/test",(req,res)=>{
@@ -11,6 +19,12 @@ app.use("/test",(req,res)=>{
 app.use("/hello",(req,res)=>{
 res.send("this is hello")
 })
+
+// app.use("/",(req,res)=>{
+// res.send("this is home ")
+// })
+
+
 app.listen(3000,()=>{
     console.log("server is running successfully on port 3000")
 })
